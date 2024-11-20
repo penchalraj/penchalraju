0738f52747d638fb26ef0db90881f144217d01be
penchalraj <penchalraghava@gmail.com>
2024-11-20 18:59:21 +0530

    Add files via upload

---
+++ p0002.java
@@ -0,0 +1,10 @@
+public class p0002 {
+    public static void main(String[] args) {
+        for(int i=100;i>=1;i--){
+       System.out.println(i);
+
+        }
+        System.out.println("happy birthday siri");
+    }
+    
+}
